58077a6266f6cdcc9182232efedd900a292219af
zhw <1573529065@qq.com>
2018-11-28 22:55:52 +0800

    采购信息表

--- database/migrations/2018_11_21_162257_create_purchases_table.php
+++ database/migrations/2018_11_21_162257_create_purchases_table.php
@@ -15,20 +15,20 @@ class CreatePurchasesTable extends Migration
         Schema::create('purchases', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('ub_id')->comment('采购者用户ub_id');
-            $table->string('name', 50)->comment('产品名');
-            $table->string('name', 20)->comment('需求省');
-            $table->string('name', 20)->comment('需求市');
-            $table->string('name', 20)->comment('需求区，县');
-            $table->integer('name')->comment('解锁采购需要金币数');
+            $table->string('product_name', 50)->comment('产品名');
+            $table->string('province', 20)->comment('需求省');
+            $table->string('city', 20)->comment('需求市');
+            $table->string('area', 20)->comment('需求区，县');
+            $table->integer('price')->comment('解锁采购需要金币数');
             $table->timestamp('push_at')->comment('发布时间');
             $table->timestamp('expire_at')->comment('过期时间');
             $table->test('detail')->comment('采购信息详情');
             $table->unsignedInteger('pay_num')->comment('付费查看次数');
 
-
-            // 还有索引外建没建，
-
-
+            $table->index('ub_id');
+            $table->index('province');
+            $table->index('city');
+            $table->index('area');
 
             $table->timestamps();
         });
